eadf0ecdc3fc53b81cd528b51680720cc5aa4296
Debrena McEwen <debrenamcewen@gmail.com>
2017-04-26 14:05:34 -0700

    add on/off toggle button

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,29 @@
+//declare global vars
+var on
+var off
+
+//onload
 document.addEventListener('DOMContentLoaded',function(event) {
-  var buttons = document.getElementsByClassName('flex-button')
-  .addEventListener('click', sortInputs)
+  on = document.getElementsByClassName('switch-on')[0];
+  off = document.getElementsByClassName('switch-off')[0];
+
+//add event listeners for functions that will turn things on and off
+  on.addEventListener('click', toggleOn);
+  off.addEventListener('click', toggleOff);
 })
+
+//functions will turn the thing on
+var toggleOn = function(event) {
+  if(on.style.display !== 'none') {
+    event.target.style.display = 'none';
+    off.style.display = 'block';
+  }
+}
+
+//functions will turn the thing off
+var toggleOff = function(event) {
+  if(off.style.display == 'block') {
+    off.style.display = 'none';
+    on.style.display = 'block'
+  }
+}
